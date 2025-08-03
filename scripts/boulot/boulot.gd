de977eaa44c370d4da1d7d6909a179e56fd5804e
acusanno <cusannoadam@gmail.com>
2025-08-03 13:55:41 +0200

    Added timer + difficulty management to boulot

--- scripts/boulot/boulot.gd
+++ scripts/boulot/boulot.gd
@@ -8,6 +8,10 @@ extends Node2D
 @onready var score_text = $Camera2D/UI/Score
 @onready var light_timer = $LightTimer
 
+var game_timer: Timer
+@onready var timerSlider = $TimerBar
+
+
 const color_dark = Color(0,0,0,0.5)
 const color_bright = Color(0,0,0,0)
 
@@ -16,16 +20,41 @@ var current_shoes : int = 0
 var quota : int = 5
 
 func _ready() -> void:
+	start(GameManager.getDayCount())
+
+func start(iteration: int):
+	# Change if you want to give more or less time per iteration
+	setupTimer(25 + 2 * iteration)
+	quota = 5 + (4 * iteration)
 	update_score_display()
 	darkness.color = color_bright
 	light_timer.start()
 	trigame.hide()
-	
+	game_timer.start()
+
 func _process(delta: float) -> void:
 	if darkness.color == color_dark:
 		Globals.is_dark = true
 	else :
 		Globals.is_dark = false
+	timerSlider.value = game_timer.time_left
+
+func setupTimer(timer_duration: float):
+	game_timer = Timer.new()
+	game_timer.wait_time = timer_duration
+	#print(game_timer.wait_time)
+	
+	timerSlider.max_value = timer_duration
+	game_timer.one_shot = true
+	game_timer.timeout.connect(_on_timer_timeout)
+	add_child(game_timer)
+
+func _on_timer_timeout():
+	gameFail()
+
+func gameFail():
+	GameManager.goToNextStage()
+	print("Game Failed - Time's up!")
 
 func set_difficulty(difficulty : int):
 	#on regarde le jour et on set
@@ -51,6 +80,7 @@ func update_score():
 	current_shoes += 1
 	update_score_display()
 	if current_shoes >= quota:
+		game_timer.stop()
 		GameManager.goToNextStage()
 	
 func update_score_display():
